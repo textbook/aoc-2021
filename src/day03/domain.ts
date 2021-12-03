e6d34a91bb72ced47749d299a43ea5b73f0c762c
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-12-03 15:48:30 +0000

    Refactor to extract similarities

--- src/day03/domain.ts
+++ src/day03/domain.ts
@@ -1,31 +1,23 @@
 export default function process(report: string[]): number {
-  const o2gen = filterByMostCommonOrOne(report);
-  const co2scrub = filterByLeastCommonOrZero(report);
+  const o2gen = findEntry(report, Method.MOST_COMMON_OR_ONE);
+  const co2scrub = findEntry(report, Method.LEAST_COMMON_OR_ZERO);
   return parseInt(o2gen, 2) * parseInt(co2scrub, 2);
 }
 
-function filterByMostCommonOrOne(report: string[], index: number = 0): string {
+enum Method {
+  MOST_COMMON_OR_ONE,
+  LEAST_COMMON_OR_ZERO,
+}
+
+function findEntry(report: string[], method: Method, index: number = 0): string {
   if (report.length === 1) {
     return report[0];
   }
-  const [ones, zeroes] = partition(report, index);
-  if (ones >= zeroes) {
-    return filterByMostCommonOrOne(report.filter((entry) => entry[index] === "1"), index + 1);
-  }
-  return filterByMostCommonOrOne(report.filter((entry) => entry[index] === "0"), index + 1);
+  const [ones, zeroes] = countBits(report, index);
+  const relevantBit = (method === Method.MOST_COMMON_OR_ONE ? ones >= zeroes : ones < zeroes) ? "1" : "0"
+  return findEntry(report.filter((entry) => entry[index] === relevantBit), method, index + 1);
 }
 
-function filterByLeastCommonOrZero(report: string[], index: number = 0): string {
-  if (report.length === 1) {
-    return report[0];
-  }
-  const [ones, zeroes] = partition(report, index);
-  if (ones < zeroes) {
-    return filterByLeastCommonOrZero(report.filter((entry) => entry[index] === "1"), index + 1);
-  }
-  return filterByLeastCommonOrZero(report.filter((entry) => entry[index] === "0"), index + 1);
-}
-
-function partition(report: string[], index: number): [number, number] {
+function countBits(report: string[], index: number): [number, number] {
   return report.reduce(([ones, zeros], entry) => entry[index] === "1" ? [ones + 1, zeros] : [ones, zeros + 1], [0, 0]);
 }
