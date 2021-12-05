8ef73ef6b516c38363f49cc0313fdec8be11d4eb
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-12-05 10:54:40 +0000

    Normalise start and end to simplify logic

--- src/day05/domain.ts
+++ src/day05/domain.ts
@@ -21,23 +21,20 @@ export default function process(lines: Line[]): number {
 
 function points(from: Coordinate, to: Coordinate): Coordinate[] {
   const coordinates: Coordinate[] = [];
-  const [x1, y1] = from;
-  const [x2, y2] = to;
+  const [[x1, y1], [x2, y2]] = normalise(from, to);
   if (x1 === x2) {
     range(y1, y2).forEach((y) => coordinates.push([x1, y]));
-  } else if (y1 === y2) {
-    range(x1, x2).forEach((x) => coordinates.push([x, y1]));
   } else {
-    if (x2 > x1) {
-      range(x1, x2).forEach((x, index) => coordinates.push([x, y1 + (index * (y2 > y1 ? 1 : -1))]));
-    } else {
-      range(x1, x2).forEach((x, index) => coordinates.push([x, y2 + (index * (y2 > y1 ? -1 : 1))]));
-    }
+    const dy = y1 === y2 ? 0 : y2 > y1 ? 1 : -1;
+    range(x1, x2).forEach((x, index) => coordinates.push([x, y1 + (index * dy)]));
   }
   return coordinates;
 }
 
-function range(value1: number, value2: number): number[] {
-  const [start, end] = [value1, value2].sort((a, b) => a - b);
+function normalise(from: Coordinate, to: Coordinate): Coordinate[] {
+  return [from, to].sort(([x1, y1], [x2, y2]) => x1 === x2 ? y1 - y2 : x1 - x2);
+}
+
+function range(start: number, end: number): number[] {
   return [...Array(end - start + 1)].map((_, index) => index + start);
 }
