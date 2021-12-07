ebd266775e0070e23ac916098c506091d9a6a663
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-12-07 08:14:52 +0000

    Day 7 - part 2

--- src/day07/domain.ts
+++ src/day07/domain.ts
@@ -1,6 +1,11 @@
 export default function process(currentPositions: number[]): number {
   const maxPosition = Math.max(...currentPositions);
   const costs = [...Array(maxPosition + 1)]
-    .map((_, targetPosition) => currentPositions.reduce((curr, next) => curr + Math.abs(next - targetPosition), 0));
+    .map((_, targetPosition) => currentPositions.reduce((curr, next) => curr + cost(next, targetPosition), 0));
   return Math.min(...costs);
+}
+
+function cost(currentPosition: number, targetPosition: number): number {
+  const distance = Math.abs(currentPosition - targetPosition);
+  return (distance * (distance + 1)) / 2;
 }
--- src/day07/index.test.ts
+++ src/day07/index.test.ts
@@ -4,6 +4,6 @@ import whaleTreachery from ".";
 
 describe("The Treachery of Whales", () => {
   it("determines the minimum fuel needed to align the crabs", () => {
-    expect(whaleTreachery("16,1,2,0,4,2,7,1,2,14")).to.equal(37);
+    expect(whaleTreachery("16,1,2,0,4,2,7,1,2,14")).to.equal(168);
   });
 });
