089d3939ff5be5bd195a62e0a84d4bbd8c85c1dd
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-12-17 13:07:10 +0000

    Day 16 - part 2

--- src/day16/domain.test.ts
+++ src/day16/domain.test.ts
@@ -3,31 +3,227 @@ import { expect } from "chai";
 import process, { Packet } from "./domain";
 
 describe("Packet Decoder domain", () => {
-  it("sums the versions in all packets", () => {
+  it("can return a value", () => {
+    expect(process(<Packet>{
+      type: 4,
+      value: 123,
+      version: 1,
+    })).to.equal(123);
+  });
+
+  it("can perform a sum operation", () => {
     expect(process(<Packet>{
       operands: [
         {
-          operands: [
-            {
-              type: 4,
-              value: 456,
-              version: 1,
-            }, {
-              type: 4,
-              value: 789,
-              version: 0,
-            },
-          ],
-          type: 2,
-          version: 2,
+          type: 4,
+          value: 456,
+          version: 1,
         }, {
           type: 4,
-          value: 123,
-          version: 8,
+          value: 789,
+          version: 0,
+        },
+      ],
+      type: 0,
+      version: 4,
+    })).to.equal(1_245);
+  });
+
+  it("can perform a product operation", () => {
+    expect(process(<Packet>{
+      operands: [
+        {
+          type: 4,
+          value: 456,
+          version: 1,
+        }, {
+          type: 4,
+          value: 789,
+          version: 0,
         },
       ],
       type: 1,
       version: 4,
-    })).to.equal(15);
+    })).to.equal(359_784);
+  });
+
+  it("can perform a minimum operation", () => {
+    expect(process(<Packet>{
+      operands: [
+        {
+          type: 4,
+          value: 456,
+          version: 1,
+        }, {
+          type: 4,
+          value: 789,
+          version: 0,
+        },
+      ],
+      type: 2,
+      version: 4,
+    })).to.equal(456);
+  });
+
+  it("can perform a maximum operation", () => {
+    expect(process(<Packet>{
+      operands: [
+        {
+          type: 4,
+          value: 456,
+          version: 1,
+        }, {
+          type: 4,
+          value: 789,
+          version: 0,
+        },
+      ],
+      type: 3,
+      version: 4,
+    })).to.equal(789);
+  });
+
+  it("can perform a greater than operation", () => {
+    expect(process(<Packet>{
+      operands: [
+        {
+          type: 4,
+          value: 456,
+          version: 1,
+        }, {
+          type: 4,
+          value: 789,
+          version: 0,
+        },
+      ],
+      type: 5,
+      version: 4,
+    })).to.equal(0);
+    expect(process(<Packet>{
+      operands: [
+        {
+          type: 4,
+          value: 789,
+          version: 1,
+        }, {
+          type: 4,
+          value: 789,
+          version: 0,
+        },
+      ],
+      type: 5,
+      version: 4,
+    })).to.equal(0);
+    expect(process(<Packet>{
+      operands: [
+        {
+          type: 4,
+          value: 789,
+          version: 1,
+        }, {
+          type: 4,
+          value: 456,
+          version: 0,
+        },
+      ],
+      type: 5,
+      version: 4,
+    })).to.equal(1);
+  });
+
+  it("can perform a less than operation", () => {
+    expect(process(<Packet>{
+      operands: [
+        {
+          type: 4,
+          value: 456,
+          version: 1,
+        }, {
+          type: 4,
+          value: 789,
+          version: 0,
+        },
+      ],
+      type: 6,
+      version: 4,
+    })).to.equal(1);
+    expect(process(<Packet>{
+      operands: [
+        {
+          type: 4,
+          value: 789,
+          version: 1,
+        }, {
+          type: 4,
+          value: 789,
+          version: 0,
+        },
+      ],
+      type: 6,
+      version: 4,
+    })).to.equal(0);
+    expect(process(<Packet>{
+      operands: [
+        {
+          type: 4,
+          value: 789,
+          version: 1,
+        }, {
+          type: 4,
+          value: 456,
+          version: 0,
+        },
+      ],
+      type: 6,
+      version: 4,
+    })).to.equal(0);
+  });
+
+  it("can perform an equality operation", () => {
+    expect(process(<Packet>{
+      operands: [
+        {
+          type: 4,
+          value: 456,
+          version: 1,
+        }, {
+          type: 4,
+          value: 789,
+          version: 0,
+        },
+      ],
+      type: 7,
+      version: 4,
+    })).to.equal(0);
+    expect(process(<Packet>{
+      operands: [
+        {
+          type: 4,
+          value: 789,
+          version: 1,
+        }, {
+          type: 4,
+          value: 789,
+          version: 0,
+        },
+      ],
+      type: 7,
+      version: 4,
+    })).to.equal(1);
+    expect(process(<Packet>{
+      operands: [
+        {
+          type: 4,
+          value: 789,
+          version: 1,
+        }, {
+          type: 4,
+          value: 456,
+          version: 0,
+        },
+      ],
+      type: 7,
+      version: 4,
+    })).to.equal(0);
   });
 });
--- src/day16/domain.ts
+++ src/day16/domain.ts
@@ -8,10 +8,18 @@ export type Packet =
   | BasePacket & { value: number };
 
 export default function process(root: Packet): number {
-  if ("operands" in root) {
-    return root.version + root.operands
-      .map((packet) => process(packet))
-      .reduce((curr, next) => curr + next, 0);
+  if ("value" in root) {
+    return root.value;
   }
-  return root.version;
+  return operations[root.type](root.operands);
 }
+
+const operations: { [type: number]: (operands: Packet[]) => number } = {
+  0: (operands) => operands.reduce((curr, next) => curr + process(next), 0),
+  1: (operands) => operands.reduce((curr, next) => curr * process(next), 1),
+  2: (operands) => Math.min(...operands.map((op) => process(op))),
+  3: (operands) => Math.max(...operands.map((op) => process(op))),
+  5: ([op1, op2]) => process(op1) > process(op2) ? 1 : 0,
+  6: ([op1, op2]) => process(op1) < process(op2) ? 1 : 0,
+  7: ([op1, op2]) => process(op1) === process(op2) ? 1 : 0,
+};
